c47890a98a96344bf99980188e357738de8c5cd8
vratislavino <vratislavino@gmail.com>
2023-04-24 09:58:16 +0200

    animation working

--- Assets/Skakacka2D/Coin2D.cs
+++ Assets/Skakacka2D/Coin2D.cs
@@ -7,14 +7,16 @@ public class Coin2D : MonoBehaviour
     Animator coinAnimator;
 
     private void Start() {
-        this.coinAnimator= GetComponentInChildren<Animator>();
+        this.coinAnimator= GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         var player = collision.GetComponent<PlayerCoins>();
         if(player != null) {
             player.AddCoin();
-            Destroy(gameObject);
+            Debug.Log("DEstroying");
+            this.coinAnimator.SetTrigger("Destroy");
+            Destroy(gameObject, 1);
         }
     }
 }
